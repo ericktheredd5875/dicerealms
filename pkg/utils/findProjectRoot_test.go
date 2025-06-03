c0d67ecd375cd5749c76e881019bb670fc1f70be
ericktheredd5875 <ericktheredd5875@gmail.com>
2025-06-02 22:24:34 -0700

    feat: added persistant character saving to postgres and better control over connection sessions.

--- pkg/utils/findProjectRoot_test.go
+++ pkg/utils/findProjectRoot_test.go
@@ -7,9 +7,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// export ASSERT_ROOT_DIR=C:/CodeBases/dicerealms
 func TestFindProjectRoot(t *testing.T) {
 
 	rootDir := os.Getenv("ASSERT_ROOT_DIR")
+	if rootDir == "" {
+		rootDir = "/home/runner/work/dicerealms/dicerealms"
+	}
 
 	root, err := FindProjectRoot("")
 	assert.NoError(t, err)
